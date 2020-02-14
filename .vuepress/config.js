347304865c01f268e57ea5527d994aaeb2251f6e
last order <2495713984@qq.com>
2020-02-14 14:36:27 +0800

    修改readme

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: last order
  * @Date: 2019-08-08 13:04:15
- * @LastEditTime : 2020-02-14 13:36:03
+ * @LastEditTime : 2020-02-14 14:23:51
  */
 const path = require('path')
 const dir = path.resolve(__dirname, '../docs')
@@ -9,8 +9,6 @@ console.log(dir)
 
 module.exports = {
   base: '/LoPlayer-Docs/',
-  title: 'LoPlayer',
-  description: '一个简易的播放器',
   dest: dir,
   themeConfig: {
     nav: [{
@@ -21,5 +19,12 @@ module.exports = {
       text: 'Github',
       link: 'https://github.com/diy4869/LoPlayer'
     }]
+  },
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'LoPlayer',
+      description: '一个简易的播放器'
+    }
   }
 }
