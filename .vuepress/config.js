1548b7bf832f5219044ad6f4374f7b4d91465616
last order <2495713984@qq.com>
2020-02-14 13:11:14 +0800

    第一次提交

---
+++ .vuepress/config.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: last order
+ * @Date: 2019-08-08 13:04:15
+ * @LastEditTime : 2020-02-14 13:01:45
+ */
+const path = require('path')
+const dir = path.resolve(__dirname, '../docs')
+console.log(dir)
+
+module.exports = {
+  title: 'LoPlayer',
+  description: '一个简易的播放器',
+  dest: dir,
+  themeConfig: {
+    nav: [{
+      text: '学习',
+      link: '/guide/'
+    },
+    {
+      text: 'Github',
+      link: 'https://github.com/diy4869/LoPlayer'
+    }]
+  }
+}
