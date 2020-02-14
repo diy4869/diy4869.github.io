2151c0a95555093c1a23117054b6c1d540389b29
last order <2495713984@qq.com>
2020-02-14 13:36:48 +0800

    修改vuepress默认路径

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: last order
  * @Date: 2019-08-08 13:04:15
- * @LastEditTime : 2020-02-14 13:01:45
+ * @LastEditTime : 2020-02-14 13:36:03
  */
 const path = require('path')
 const dir = path.resolve(__dirname, '../docs')
 console.log(dir)
 
 module.exports = {
+  base: '/LoPlayer-Docs/',
   title: 'LoPlayer',
   description: '一个简易的播放器',
   dest: dir,
